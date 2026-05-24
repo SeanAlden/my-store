302be2b157b3fc33382a53d85c1798687f9a7acc
Sean Alden <c14210052@john.petra.ac.id>
2026-05-24 21:03:26 +0700

    update database configuration

--- config/database.go
+++ config/database.go
@@ -3,21 +3,37 @@ package config
 import (
 	"database/sql"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var DB *sql.DB
 
 func ConnectDB() {
-	dsn := "root:@tcp(127.0.0.1:3306)/gycora_database?parseTime=true"
-	var err error
-	DB, err = sql.Open("mysql", dsn)
+	// load .env file
+	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error opening database: ", err)
+		log.Println("No .env file found, using system env")
+	}
+
+	// ambil env
+	dsn := os.Getenv("MYSQL_DSN")
+
+	if dsn == "" {
+		log.Fatal("MYSQL_DSN is empty")
+	}
+
+	var errDB error
+	DB, errDB = sql.Open("mysql", dsn)
+	if errDB != nil {
+		log.Fatal("Error opening database: ", errDB)
 	}
 
 	if err = DB.Ping(); err != nil {
 		log.Fatal("Error connecting to database: ", err)
 	}
+
+	log.Println("Database connected successfully")
 }
--- go.mod
+++ go.mod
@@ -6,5 +6,7 @@ require (
 	filippo.io/edwards25519 v1.2.0 // indirect
 	github.com/go-sql-driver/mysql v1.9.3 // indirect
 	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.12.3 // indirect
 	golang.org/x/crypto v0.49.0 // indirect
 )
